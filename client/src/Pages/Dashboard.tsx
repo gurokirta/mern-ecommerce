0fe82d1f1ee632e0f8d79c1461ea9fd99932bb76
Guro <70147494+gurokirta@users.noreply.github.com>
2024-03-06 16:23:38 +0400

    Refactor color state variable in Dashboard component

--- client/src/Pages/Dashboard.tsx
+++ client/src/Pages/Dashboard.tsx
@@ -47,7 +47,7 @@ export default function Dashboard() {
     userRef: currentUser?._id,
   });
 
-  const [color, setColors] = useState([
+  const [colors, setColors] = useState([
     {
       name: "bg-primary",
       active: false,
@@ -73,7 +73,7 @@ export default function Dashboard() {
 
   console.log(product);
   const handleSetActiveColor = (ItemId: string) => {
-    const updatedColors = color.map((color) => {
+    const updatedColors = colors.map((color) => {
       return {
         ...color,
         active: color.name === ItemId ? !color.active : color.active,
@@ -86,7 +86,7 @@ export default function Dashboard() {
 
     setProduct((prev) => ({
       ...prev,
-      colors: colorNames,
+      color: colorNames,
     }));
 
     setColors(updatedColors);
@@ -410,7 +410,7 @@ export default function Dashboard() {
           Please choose color:{" "}
         </h2>
         <div className="flex gap-3">
-          {color.map((color) => (
+          {colors.map((color) => (
             <div
               key={color.name}
               onClick={() => handleSetActiveColor(color.name)}
@@ -419,7 +419,7 @@ export default function Dashboard() {
           ))}
         </div>
         <div className="flex gap-2 w-64 h-12  rounded-lg border-neutral-05 border px-3 py-1 ">
-          {color.map(
+          {colors.map(
             (color) =>
               color.active && (
                 <div
