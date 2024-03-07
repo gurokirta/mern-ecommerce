a02006522d143eb80c0bc8dde3e1461fae995b4d
Guro <70147494+gurokirta@users.noreply.github.com>
2024-03-07 10:41:07 +0400

    Refactor ProductItem component layout and styles

--- client/src/Pages/ProductItem.tsx
+++ client/src/Pages/ProductItem.tsx
@@ -12,60 +12,61 @@ export default function ProductItem() {
   return (
     <>
       <div className="max-w-lg mx-auto flex gap-8">
-        <div className="flex flex-col gap-2 w-[268px] ">
+        <div className="flex flex-col gap-2">
           {data && (
             <img
               src={data.pictures[0]}
               alt="pic"
-              className="w-[268px] h-[328px] object-cover"
+              className="w-96 object-cover h-[500px]"
             />
           )}
-          <div className="flex w-[547px]  ">
+          <div className="flex w-96 gap-2">
             {data?.pictures.map((pic) => (
               <img
                 src={pic}
                 alt="pic"
-                className="w-[84px] h-[90px] mr-2 object-cover"
+                className="w-1/2 h-[90px]  object-cover"
               />
             ))}
           </div>
         </div>
-        <div>
-          <div className="flex flex-col gap-2 ">
-            {data?.reviews?.map((review) => <p> {review.like} Reviews</p>)}
-            <h1 className="text-heading-05 text-neutral-07"> {data?.title}</h1>
-            {data?.description}
-            <div className="flex gap-2 border-b border-neutral-03 pb-4">
-              <p className="text-heading-06 text-neutral-07">
-                {" "}
-                ${data?.discountedPrice.toFixed(2)}
-              </p>
+
+        <div className="flex flex-col gap-2 w-96 justify-between">
+          {data?.reviews?.map((review) => <p> {review.like} Reviews</p>)}
+          <h1 className="text-heading-05 text-neutral-07"> {data?.title}</h1>
+          <p className="text-regular-06 text-neutral-04">{data?.description}</p>
+          <div className="flex gap-2 border-b border-neutral-03 pb-4">
+            <p className="text-heading-06 text-neutral-07">
+              {" "}
+              ${data?.discountedPrice.toFixed(2)}
+            </p>
+            {data?.offer && (
               <p className="text-heading-06 text-neutral-04 line-through">
                 ${data?.price.toFixed(2)}
               </p>
-            </div>
-            <div className="flex flex-col mt-4 gap-2 ">
-              <span className="text-neutral-04 text-regular-04">
-                Measurements
-              </span>
-              {data?.measurements}
-            </div>
-            <div>
-              <span className="text-neutral-04 text-regular-04">
-                Choose Color
-              </span>
-              <div className="flex gap-2">
-                {data?.color?.map((color) => (
-                  <div className={`w-8 h-8 rounded-full ${color}`}></div>
-                ))}
-              </div>
+            )}
+          </div>
+          <div className="flex flex-col mt-4 gap-2 ">
+            <span className="text-neutral-04 text-regular-04">
+              Measurements
+            </span>
+            {data?.measurements}
+          </div>
+          <div className="flex flex-col gap-2">
+            <span className="text-neutral-04 text-regular-04 ">
+              Choose Color {`>`}
+            </span>
+            <div className="flex gap-2">
+              {data?.color?.map((color) => (
+                <div className={`w-8 h-8 rounded-full ${color}`}></div>
+              ))}
             </div>
           </div>
           <div>
             {currentUser?.isAdmin ? (
               <div>
                 <p className="text-secondary-red">Admin is here</p>
-                <button className="bg-primary py-2 px-12 border rounded-lg text-neutral-01">
+                <button className="bg-primary py-2 px-12 border rounded-lg text-neutral-01 w-full">
                   Edit
                 </button>
               </div>
