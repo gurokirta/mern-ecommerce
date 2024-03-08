3c5290cdfa50e3975af8764c94ca41cc374d00ec
Guro <70147494+gurokirta@users.noreply.github.com>
2024-03-08 10:09:30 +0400

    Add navigation arrows to ProductItem component

--- client/src/Pages/ProductItem.tsx
+++ client/src/Pages/ProductItem.tsx
@@ -2,6 +2,8 @@ import { Link, useParams } from "react-router-dom";
 import { useGetProductQuery } from "../Redux/services/fetchData";
 import { useSelector } from "react-redux";
 import { RootState } from "../Redux/Store";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+import { FaArrowRightLong, FaArrowLeftLong } from "react-icons/fa6";
 
 export default function ProductItem() {
   const params = useParams();
@@ -11,8 +13,8 @@ export default function ProductItem() {
 
   return (
     <>
-      <div className="max-w-lg mx-auto flex gap-8">
-        <div className="flex flex-col gap-2">
+      <div className="max-w-2xl mx-auto flex gap-10">
+        <div className="flex flex-col gap-2 relative">
           {data && (
             <img
               src={data.pictures[0]}
@@ -20,6 +22,10 @@ export default function ProductItem() {
               className="w-96 object-cover h-[500px]"
             />
           )}
+          <div className="flex justify-between absolute w-full mx-10">
+            <FaArrowLeftLong className="bg-neutral-02 text-neutral-05 rounded-2xl w-3" />
+            <FaArrowRightLong className="bg-neutral-02 text-neutral-05 rounded-2xl w-3" />
+          </div>
           <div className="flex w-96 gap-2">
             {data?.pictures.map((pic) => (
               <img
@@ -31,10 +37,18 @@ export default function ProductItem() {
           </div>
         </div>
 
-        <div className="flex flex-col gap-2 w-96 justify-between">
-          {data?.reviews?.map((review) => <p> {review.like} Reviews</p>)}
-          <h1 className="text-heading-05 text-neutral-07"> {data?.title}</h1>
-          <p className="text-regular-06 text-neutral-04">{data?.description}</p>
+        <div className="flex flex-col gap-2 w-1/2 justify-between">
+          <div>
+            {data?.reviews?.map((review) => <p> {review.like} Reviews</p>)}
+          </div>
+          <div>
+            <h1 className="text-heading-05 text-neutral-07"> {data?.title}</h1>
+          </div>
+          <div>
+            <p className="text-regular-06 text-neutral-04">
+              {data?.description}
+            </p>
+          </div>
           <div className="flex gap-2 border-b border-neutral-03 pb-4">
             <p className="text-heading-06 text-neutral-07">
               {" "}
@@ -78,6 +92,12 @@ export default function ProductItem() {
             )}
           </div>
         </div>
+      </div>
+      <FaArrowAltCircleLeft className="bg-neutral-03 text-secondary-red" />
+      <FaArrowAltCircleRight />
+      <div className="flex justify-between absolute">
+        <FaArrowLeftLong className="bg-neutral-02 text-neutral-05 rounded-2xl w-3" />
+        <FaArrowRightLong className="bg-neutral-02 text-neutral-05 rounded-2xl w-3" />
       </div>
     </>
   );
